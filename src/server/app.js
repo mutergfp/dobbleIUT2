cf4aa6f47002fa38fa51e7bc1ad7f27dfcc72d83
Maël Gaonach <mael@gaonach.com>
2018-03-16 09:52:12 +0100

    Allow cross domain

--- src/server/app.js
+++ src/server/app.js
@@ -14,6 +14,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Cross origin
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // cdn middleware
 app.use('/download/', express.static(path.join(process.cwd(), 'dist')));
 app.use('/download/assets', express.static(path.join(process.cwd(), 'assets')));
