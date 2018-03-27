609efdd7541938fa5dc72433a130a08a9898a9d0
Maël Gaonach <mael@gaonach.com>
2018-03-27 08:43:48 +0200

    fix URL

--- src/server/services/game/logs.js
+++ src/server/services/game/logs.js
@@ -2,6 +2,6 @@ import { fetchingJSON } from '@server/utils/fetching';
 
 const BASE_URL = 'http://gi1.univ-lr.fr:9200';
 
-export const logPlayerMatch = obj => fetchingJSON(`${BASE_URL}/parties/partie?pretty`, obj, 'POST');
+export const logPlayerMatch = obj => fetchingJSON(`${BASE_URL}/parties/partie`, obj, 'POST');
 
-export const logClick = obj => fetchingJSON(`${BASE_URL}/clics/clic?pretty/`, obj, 'POST');
+export const logClick = obj => fetchingJSON(`${BASE_URL}/clics/clic`, obj, 'POST');
